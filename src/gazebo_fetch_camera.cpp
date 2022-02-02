282aedb3f5912f7a22c07a7097306b4bf537b9b9
Keshav Kapur <keshav29kapur@gmail.com>
2022-02-02 12:23:35 +0530

    Comments Updated

--- src/gazebo_fetch_camera.cpp
+++ src/gazebo_fetch_camera.cpp
@@ -1,32 +1,33 @@
-#include <gazebo-10/gazebo/gazebo_client.hh>
-#include <gazebo-10/gazebo/transport/transport.hh>
-#include <gazebo-10/gazebo/gazebo.hh>
-#include <gazebo-10/gazebo/msgs/msgs.hh>
-#include <gazebo-10/gazebo/transport/TransportTypes.hh>
+//	Name: Keshav Kapur
+//	Email: keshav29kapur@gmail.com
+// Gazebo Libraries 
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/TransportTypes.hh>
 #include <gazebo/msgs/image_stamped.pb.h>
+
+// Standard Libraries
 #include <iostream>
+
+// OpenCV Libraries
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+
 //// Function is called everytime a message is received.
-
 void camera_callback(ConstImageStampedPtr &msg){
-//  // Dump the message contents to stdout.
-    //int width, height;
-    //char* data;
-
-    //width =  (int) msg->image().width();
-    //height = (int) msg->image().height();
-    //data = new char[msg->image().data().length() + 1];
-    //memcpy(data, msg->image().data().c_str(), msg->image().data().length());
-    
-
+	
+//	// Setting up OpenCV image container to store the camera feedback
     	cv::Mat mRGBAImg = cv::Mat(cv::Size(msg->image().width(),msg->image().height()),CV_8UC3);
+
+//	// Saving Camera Feedback Data to the image container
 	memcpy(mRGBAImg.data,msg->image().data().c_str(),msg->image().data().size());
+
+//	// Image Display Video Feedback
 	cv::imshow("Camera Feed", mRGBAImg);
 	cv::waitKey(3);
 
-    //cv::Mat image(height, width, CV_8UC3, data);
-    //delete data;
 }
 
 int main(int _argc, char **_argv){
@@ -36,13 +37,15 @@ int main(int _argc, char **_argv){
 //      // Create our node for communication
         gazebo::transport::NodePtr node(new gazebo::transport::Node());
         node->Init();
-
-//                // Listen to Gazebo world_stats topic
+	
+	std::clog << "Client Connected!\n";
+//      // Listen to Gazebo world_stats topic
         gazebo::transport::SubscriberPtr sub = node->Subscribe("/gazebo/default/distorted_camera/link/camera/image", camera_callback);
 
-//                    // Busy wait loop...replace with your own code as needed.
+//      // Busy wait loop
 	while(1){
 		gazebo::common::Time::MSleep(10);
 	}
+//	// Node shutdown
         gazebo::client::shutdown();
 }
